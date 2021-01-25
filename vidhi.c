06d1a444b75e52dce71ef8e5caf0969e7bae077a
vidhisinha2002 <72182782+vidhisinha2002@users.noreply.github.com>
2021-01-25 14:38:48 +0530

    Create vidhi.c

---
+++ vidhi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+/* vidhi sinha------vidhisinha2002@gmail.com*/
+int main()
+{
+    int t,n,ans,m;
+    scanf ("%d",&t);
+    while (t--)
+    {
+        scanf ("%d %d",&n,&m);
+        ans=n+m;
+        printf ("%d\n",ans);
+    }
+    return 0;
+}
