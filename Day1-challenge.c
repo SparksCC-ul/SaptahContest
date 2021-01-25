780b068088c22eecb8ed44338209e70ce57f461c
Rahilcode <73123454+Rahilcode@users.noreply.github.com>
2021-01-25 11:42:25 +0530

    Day-1 challenge Solution

---
+++ Day1-challenge.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main(){
+    int inp[100], a, b, n, sum;
+    scanf("%d", &n);
+    int i =0;
+    while(i<n){
+        scanf("%d %d", &inp[i+i], &inp[i+i+1]);
+        i++;
+    }
+    int j=0;
+    while(j<n){
+        sum = inp[j+j] + inp[j+j+1];
+        printf("\n%d", sum);
+        j++;
+    }
+
+}
+
+/* Mohammed Raheel
+   raheelkhatttal@gmail.com*/
