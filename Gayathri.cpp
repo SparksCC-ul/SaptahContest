07694272f82f7d2817d13d8073a73b959a17e2a0
GAYATHRI S <69425160+GAYATHRI-SURESH-S@users.noreply.github.com>
2021-01-29 17:20:39 +0530

    Create Gayathri.cpp

---
+++ Gayathri.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+
+bool areBracketsBalanced(string expr) 
+{   
+    stack<char> s; 
+    char x; 
+  
+ 
+    for (int i = 0; i < expr.length(); i++)  
+    { 
+        if (expr[i] == '(' || expr[i] == '['
+            || expr[i] == '{')  
+        { 
+            
+            s.push(expr[i]); 
+            continue; 
+        } 
+  
+      
+    if (s.empty()) 
+            return false; 
+  
+        switch (expr[i]) { 
+        case ')': 
+            x = s.top(); 
+            s.pop(); 
+            if (x == '{' || x == '[') 
+                return false; 
+            break; 
+  
+        case '}': 
+  
+            
+            x = s.top(); 
+            s.pop(); 
+            if (x == '(' || x == '[') 
+                return false; 
+            break; 
+            case ']': 
+  
+            
+            x = s.top(); 
+            s.pop(); 
+            if (x == '(' || x == '{') 
+                return false; 
+            break; 
+        } 
+    } 
+  
+ 
+    return (s.empty()); 
+}
+int main() 
+{ 
+    cin>> expr;
+  
+    
+    if (areBracketsBalanced(expr)) 
+        cout << "true"; 
+    else
+        cout << "Not false"; 
+    return 0; 
+} 
